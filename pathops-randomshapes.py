0e81f568a088c0f3fcd54482028277e41f6a138e
justvanrossum <justvanrossum@gmail.com>
2021-10-23 11:25:09 +0200

    try it

---
+++ pathops-randomshapes.py
@@ -0,0 +1,32 @@
+from random import random, randint, seed
+from pathops import Path, PathVerb
+from pathops.operations import union
+
+
+def randCoord():
+    return randint(0, 800), randint(0, 800)
+
+def randShape(pen):
+        pen.moveTo(randCoord())
+        for k in range(3, 8):
+            if random() > 10.7:
+                pen.curveTo(randCoord(), randCoord(), randCoord())
+            else:
+                pen.lineTo(randCoord())
+        pen.closePath()
+
+
+seed(0)
+
+for i in range(100):
+    print("---", i)
+
+    path = []
+    for j in range(randint(2, 4)):
+        sub = Path()
+        randShape(sub.getPen())
+        path.append(sub)
+
+    result = Path()
+    union(path, result.getPen())
+    # print(result)
